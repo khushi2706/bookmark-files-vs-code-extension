f1e39ea57896f379aae0efd2768be4b1dcf3d922
Khushi patel <kdpatel@ibaset.com>
2025-04-25 09:24:10 +0530

    bookmark files extension created

--- extension.js
+++ extension.js
@@ -1,36 +1,73 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+class BookmarkProvider {
+	constructor() {
+		this.bookmarks = [];
+		this._onDidChangeTreeData = new vscode.EventEmitter();
+		this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+	}
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
+	refresh() {
+		this._onDidChangeTreeData.fire();
+	}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bookmark-files" is now active!');
+	addBookmark(filePath) {
+		if (!this.bookmarks.includes(filePath)) {
+			this.bookmarks.push(filePath);
+			this.refresh();
+		}
+	}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('bookmark-files.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	removeBookmark(filePath) {
+		this.bookmarks = this.bookmarks.filter(bookmark => bookmark !== filePath);
+		this.refresh();
+	}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from bookmark-files!');
-	});
+	// must implement this method 
+	getTreeItem(element) {
+		const fileName = path.basename(element);
+		const treeItem = new vscode.TreeItem(fileName);
+		treeItem.iconPath = new vscode.ThemeIcon('file'); // Add a file icon
+		treeItem.tooltip = element; // Show the full path as a tooltip
 
-	context.subscriptions.push(disposable);
+		treeItem.command = {
+			command: 'vscode.open',
+			title: 'Open File',
+			arguments: [vscode.Uri.file(element)]
+		};
+		treeItem.contextValue = 'bookmark';
+		return treeItem;
+	}
+
+	getChildren() {
+		return this.bookmarks;
+	}
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+function activate(context) {
+	const bookmarkProvider = new BookmarkProvider();
+	vscode.window.registerTreeDataProvider('bookmarkExplorer', bookmarkProvider);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('bookmark-files.addBookmark', () => {
+			const activeEditor = vscode.window.activeTextEditor;
+			if (activeEditor) {
+				bookmarkProvider.addBookmark(activeEditor.document.fileName);
+			}
+		}),
+		vscode.commands.registerCommand('bookmark-files.removeBookmark', (filePath) => {
+			bookmarkProvider.removeBookmark(filePath || vscode.window.activeTextEditor.document.fileName);
+		})
+	);
+
+
+
+}
+
+function deactivate() { }
 
 module.exports = {
 	activate,
 	deactivate
-}
+};
